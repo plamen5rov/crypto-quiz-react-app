1b15bfd8ecea071547b090d85d2801c6b1894aca
Plamen Petrov <biznetmen@gmail.com>
2021-03-23 17:03:37 +0200

    Cleared default files and added basic components

---
+++ src/components/navigation/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Header</h2>
+            </div>
+        )
+    }
+}
+
+export default Header
