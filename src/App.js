0cd20596f148e67bae88c12a71749b54da104392
Plamen Petrov <biznetmen@gmail.com>
2021-03-25 17:44:33 +0200

    Added blank page components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 //import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/navigation/Header";
-import Footer from "./components/navigation/Footer";
-import Home from "./components/pages/Home"
+import Header from "./components/navigation/Header/Header";
+import Footer from "./components/navigation/Footer/Footer";
+import Home from "./components/pages/Home/Home";
+import About from "./components/pages/About/About";
+import Categories from "./components/pages/Categories/Categories";
+import HallOfFame from "./components/pages/HallOfFame/HallOfFame";
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      
       <Home />
+      <About />
+      <Categories />
+      <HallOfFame />
+
       <Footer />
     </div>
   );
---
+++ src/components/pages/HallOfFame/HallOfFame.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HallOfFame() {
+    return (
+        <div>
+            <h1>Hall of Fame Page</h1>
+        </div>
+    )
+}
