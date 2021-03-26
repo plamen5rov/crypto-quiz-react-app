9cf28f42119cf6e8994698922d681da7c8572701
Plamen Petrov <biznetmen@gmail.com>
2021-03-26 19:09:09 +0200

    App structure changed and more components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 //import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/navigation/Header/Header";
-import Footer from "./components/navigation/Footer/Footer";
+import Header from "./components/shared/Header/Header";
+import Footer from "./components/shared/Footer/Footer";
 import Home from "./components/pages/Home/Home";
 import About from "./components/pages/About/About";
 import Categories from "./components/pages/Categories/Categories";
 import HallOfFame from "./components/pages/HallOfFame/HallOfFame";
+import Register from "./components/pages/Register/Register";
+import Login from "./components/pages/Login/Login";
+import UserProfile from "./components/user/UserProfile/UserProfile";
 
 
 function App() {
@@ -17,6 +20,9 @@ function App() {
       <About />
       <Categories />
       <HallOfFame />
+      <Register />
+      <Login />
+      <UserProfile />
 
       <Footer />
     </div>
---
+++ src/components/pages/Register/Register.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Register() {
+    return (
+        <div>
+            <h2>Register form</h2>
+        </div>
+    )
+}
