9e10cc23834ae1ae45023e084a9bfc1bb327d113
Plamen Petrov <biznetmen@gmail.com>
2021-03-28 10:35:00 +0300

    Register and Login routes added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from "./components/pages/Home/Home";
 import About from "./components/pages/About/About";
 import Categories from "./components/pages/Categories/Categories";
 import HallOfFame from "./components/pages/HallOfFame/HallOfFame";
+import Register from "./components/pages/Register/Register";
+import Login from "./components/pages/Login/Login";
 
 import { Route, Switch } from "react-router-dom";
 
@@ -13,11 +15,14 @@ function App() {
   return (
     <div className="App">
       <Header />
+
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
         <Route path="/categories" component={Categories} />
         <Route path="/halloffame" component={HallOfFame} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
         <Route render={() => <h1>Page not found!</h1>} />
       </Switch>
 
