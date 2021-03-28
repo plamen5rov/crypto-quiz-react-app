f41b249e5c396e4f5738243cd1b9555638ecadce
Plamen Petrov <biznetmen@gmail.com>
2021-03-28 10:19:59 +0300

    Page not found added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from "./components/pages/About/About";
 import Categories from "./components/pages/Categories/Categories";
 import HallOfFame from "./components/pages/HallOfFame/HallOfFame";
 
-import { Route, Link, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
         <Route path="/about" component={About} />
         <Route path="/categories" component={Categories} />
         <Route path="/halloffame" component={HallOfFame} />
+        <Route render={() => <h1>Page not found!</h1>} />
       </Switch>
 
       <Footer />
