2f5db644f9f21a4b6ec8859ba98f68504a14d447
Plamen Petrov <biznetmen@gmail.com>
2021-03-29 12:16:39 +0300

    Home page text added

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react'; // eslint-disable-next-line
+import style from "./Home.module.css";
+
 
 export class Home extends Component {
     render() {
         return (
-            <div>
-                <h1>HOME PAGE</h1>
+            <div className="home-wrapper">
+                <article className="left-box">
+            <h1>CRYPTO QUIZ</h1>
+            <h2>TEST YOUR KNOLEDGE NOW</h2>
+            <h3>
+                <p>10 CATEGORIES WITH 10 QUESTIONS EACH</p>
+                <p>REGISTER NOW AND START PLAYING</p>
+                <p>TO PUT YOUR NAME IN THE HALL OF FAME!</p>
+            </h3>
+            <button className="home-btn-register">REGISTER NOW</button>
+                </article>
+                <article className="right-box">
+                    <img src="bitcoin-coin.png" alt="bitcoin" className="home-image"/>
+                </article>
             </div>
         )
     }
