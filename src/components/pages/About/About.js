6aa7e9608372200e8d141811c38c59ea3ab13cbc
Plamen Petrov <biznetmen@gmail.com>
2021-03-30 17:30:48 +0300

    Ant Design package removed

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react';
+import './About.css'
 
-export default function About() {
-    return (
-        <div>
-            <h1>About Page</h1>
-        </div>
-    )
+export class About extends Component {
+    render() {
+        return (
+            <div className="about-page">
+                <h1>About Page</h1>
+            </div>
+        )
+    }
 }
+
+export default About
+
+
+
+
