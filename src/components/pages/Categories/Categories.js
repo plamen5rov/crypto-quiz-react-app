8ac82f088926680f3ebc5609b66b5b6eb3e39050
Plamen Petrov <biznetmen@gmail.com>
2021-03-30 18:14:31 +0300

    More styling added

--- src/components/pages/Categories/Categories.js
+++ src/components/pages/Categories/Categories.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Categories() {
     return (
         <div>
-            <h1>Categories</h1>
+            <h1 style={{padding: 50}}>Categories</h1>
         </div>
     )
 }
