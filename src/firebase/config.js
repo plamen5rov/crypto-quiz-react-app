67223ea73bc7973a1b29bcdad577adeb8c1116b6
Plamen Petrov <biznetmen@gmail.com>
2021-03-31 12:47:37 +0300

    Installing firebase and adding a config file

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAc44vd6GNDJRcuCsFXbaEcwqeSkDe_TK4",
+    authDomain: "crypto-quiz-react-app.firebaseapp.com",
+    projectId: "crypto-quiz-react-app",
+    storageBucket: "crypto-quiz-react-app.appspot.com",
+    messagingSenderId: "584292717939",
+    appId: "1:584292717939:web:9b1e3366232bfe53536106"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+
+  export {projectStorage, projectFirestore};
