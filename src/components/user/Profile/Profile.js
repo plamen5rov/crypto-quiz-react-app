f8ffdb80fac18678bc0d0f99d47be96de4cb09b2
Plamen Petrov <biznetmen@gmail.com>
2021-04-05 17:18:21 +0300

    Adding styles to the profile component

--- src/components/user/Profile/Profile.js
+++ src/components/user/Profile/Profile.js
@@ -1,15 +1,20 @@
-import React from "react";
-import ImageGrid from "../ImageGrid";
-import UploadAvatar from "../UploadAvatar/UploadAvatar"; 
+import React, { useState } from "react";
+import ImageGrid from "./ImageGrid";
+import UploadAvatar from "./UploadAvatar/UploadAvatar"; 
 import styles from "./Profile.module.css";
+import Modal from "./Modal"
 
 function Profile() {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className={styles.profileWrapper}>
       <h1 style={{ padding: 50 }}>User Profile Page</h1>
       <UploadAvatar />
       <p>Image Grid:</p>
       <ImageGrid />
+      { selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
