44b7c76f7249b93eb70dcb26fd676e194a561037
Plamen Petrov <biznetmen@gmail.com>
2021-04-06 18:54:34 +0300

    Dynamically getting categories from firebase and showing it

--- src/components/pages/Categories/Categories.js
+++ src/components/pages/Categories/Categories.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import firebase from "firebase/app";
+//import useFirestore from "hooks/useFirestore";
+import styles from "./Categories.module.css";
+//import { motion } from "framer-motion";
 
-export default function Categories() {
-    return (
-        <div>
-            <h1 style={{padding: 50}}>Categories</h1>
-        </div>
-    )
+function Categories() {
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore();
+      const data = await db.collection("categories").get();
+      setCategory(data.docs.map((doc) => doc.data()));
+    };
+
+    fetchData();
+
+    return () => {
+      setCategory([]);
+    };
+  }, []);
+
+  //const { docs } = useFirestore("categories");
+  return (
+    <div>
+      <h1 style={{ padding: 50 }}>Categories</h1>
+
+      <ol>
+        {category.map((category) => (
+          <li key={category.id}>
+            {category.id}
+            {category.name}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
+export default Categories;
